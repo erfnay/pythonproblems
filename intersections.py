5792340a789691c56b754fa697f33eff72b37f73
erfnay <erfnay@gmail.com>
2016-06-06 20:10:34 -0600

    Made intersections.py faster, better described in README

--- intersections.py
+++ intersections.py
@@ -1,39 +1,41 @@
 # Finds intersections of 2 incrementing/decrementing lists
 
-# converts inputs into more useable integer format
-def convert (list):
+# splits inputs into more useable integer format
+def split (list):
     list = list.split()
-    for i in range (0, len(list)):
-        list[i] = int(list[i])
-
     return list
+
+def convert (point):
+    point = int(point)
+
+    return point
 
 # get inputs
 inc = raw_input("Input an incrementing list: ")
 dec = raw_input("Input a decrementing list: ")
 
-# Converts strings into numbers
-inc = convert (inc)
-dec = convert (dec)
+# Splits the inputs into distinct points
+inc = split (inc)
+dec = split (dec)
 
 i = 0
 count = 0
 
 # Loops maximum of 50 times to save time on determining no intersections
 while True:
-    if inc[i] < dec[i]:
+    if convert(inc[i]) < convert(dec[i]):
         i = int(round((i + len(dec)) / 2))
 
 
-    elif inc[i] > dec[i]:
+    elif convert(inc[i]) > convert(dec[i]):
         i = int(round((i / 2)))
 
-    elif inc[i] == dec[i]:
+    elif convert(inc[i]) == convert(dec[i]):
             print "Intersection at item", (i + 1)
             break
 
     count += 1
 
     if count >= 50:
-        print "There is no intersection"
+        print "No intersection found"
         break
